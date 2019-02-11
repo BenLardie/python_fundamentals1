a4d835772ab21a3465d853a44374a0509f15741e
Ben Lardie <benlardie@gmail.com>
2019-02-11 14:47:07 -0500

    worked out birthday exercise

---
+++ excersie3.py
@@ -0,0 +1,16 @@
+# print('What is your name?')
+# user_name = input()
+# print('Hello, {}'.format(user_name))
+#
+# secret_password = 'please'
+#
+# print("What's the password?")
+# password_attempt = input()
+#
+# corect_or_not = (password_attempt == secret_password)
+#
+# print("That's {}!".format(corect_or_not))
+print("what year were you born?")
+birth_year = input()
+
+print("You are {} years old!".format(2019-int(birth_year)))
