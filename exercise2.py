dfe443a11d618ecc9fcc94c4d3d0bb956c66e2e2
Ben Lardie <benlardie@gmail.com>
2019-02-11 14:24:18 -0500

    exercise 2 complete

---
+++ exercise2.py
@@ -0,0 +1,4 @@
+tip = 0.2 * 55
+s = "A good tip is {}".format(tip)
+print('math is hard python makes it easy and lets you know 45628 times 7839 is {}'.format(45628 * 7839))
+(10 < 20 and 30 < 20) or not(10 == 11)
