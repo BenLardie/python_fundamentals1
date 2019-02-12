39e78d1dfb87119d4fc93668134132e9b5c7d00d
Ben Lardie <benlardie@gmail.com>
2019-02-11 21:31:59 -0500

    added a eat function but still getting a weird loop sometime

--- exercise5.py
+++ exercise5.py
@@ -1,14 +1,25 @@
 distance = 0
+energy = 10
 
 while distance >= 0:
     print("Would you like to walk or run?")
     travel_method = input()
-    if travel_method == "walk":
+    if energy <= 0:
+        energy += 1
+        print("used up a lot of energy lets walk, eat or go home")
+    elif travel_method == "walk":
         distance += 1
+        energy += 5
         print("distance from home is {}km".format(distance))
+        print("energy level is {}".format(energy))
     elif travel_method == "run":
-        distance +=5
+        distance += 5
+        energy -= 5
         print("distance from home is {}km".format(distance))
+        print("energy level is {}".format(energy))
+    elif travel_method == "eat":
+        energy +=10
+        print("so delicious your energy level is now {}".format(energy))
     elif travel_method == "go home":
         print("well get out of here")
         break
